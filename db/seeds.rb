9bd4f732c298546f4d260bd9be59ebb1e879fc83
kousuke takahashi <takahashi@gengosha.co.jp>
2019-03-09 19:54:45 +0900

    finished 10th_section except test FILL_IN

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ User.create!(
     name: "Example User",
     email: "example11@railstutorial.org",
     password: "foobar", 
-    password_confirmation: "foobar"
+    password_confirmation: "foobar",
+    admin: true
     )
 
 99.times do |n|
