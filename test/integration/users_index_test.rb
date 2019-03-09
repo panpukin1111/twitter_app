a16052081e6c72144b48d89a4b6f59626bd1661f
kousuke takahashi <takahashi@gengosha.co.jp>
2019-03-09 23:51:47 +0900

    finished 10th_section

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,15 +6,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  # test "index including pagination" do
-  #   log_in_as(@user)
-  #   get users_path
-  #   assert_template 'users/index'
-  #   assert_select 'div.pagination', 2
-  #   User.paginate(page: 1).each do |user|
-  #     assert_select 'a[href=?]', user_path(user), text: user.name
-  #   end
-  # end
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
@@ -23,7 +14,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
+      unless user == @admin  # 自分自身は削除しなくていい、削除リンクは出ない
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
@@ -32,9 +23,9 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-test "index as non-admin" do
-  log_in_as(@non_admin)
-  get users_path
-  assert_select 'a', text: 'delete', count: 0
-end
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
 end
