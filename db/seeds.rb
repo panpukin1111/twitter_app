d0220bb80c99adc2b72dce621e846c9cdebc98ac
kousuke takahashi <takahashi@gengosha.co.jp>
2019-03-11 17:47:36 +0900

    mailer 送信メールテストまで

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,9 @@ User.create!(
     email: "example11@railstutorial.org",
     password: "foobar", 
     password_confirmation: "foobar",
-    admin: true
+    admin: true,
+    activated: true,
+    activated_at: Time.zone.now
     )
 
 99.times do |n|
@@ -15,6 +17,8 @@ User.create!(
     name:  name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
     )
 end
