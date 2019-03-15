65b2d91b2fced91b80cb2d217de7010bd118447f
kousuke takahashi <takahashi@gengosha.co.jp>
2019-03-15 16:34:28 +0900

    finished 12th_section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'      # rails g のときにデフォルトで作成された この２つはいらない？
   get 'users/new'
   root 'static_pages#home'
@@ -13,5 +15,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
