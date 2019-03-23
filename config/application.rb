82ecbae5f2688c1dfbb5d3ae974b1df6d92c85c8
kousuke takahashi <takahashi@gengosha.co.jp>
2019-03-23 12:19:40 +0900

    heroku にあげるときのエラー対処#

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module HelloApp
     # the framework and any gems in your application.
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
   end
 end
